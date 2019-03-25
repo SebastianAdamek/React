502fe268de3378243bdd8442bd0a84dbbe9ac7c3
Sebastian Adamek <sebadamek@wp.pl>
2019-03-25 22:10:35 +0100

    add new file to the Project

---
+++ app.js
@@ -0,0 +1,25 @@
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+class Person extends React.Component {
+
+    render() {
+
+    }
+}
+
+ReactDOM.render(<List />, document.getElementById('root'));
